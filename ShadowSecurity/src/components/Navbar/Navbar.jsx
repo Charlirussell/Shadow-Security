d4da645f5be2825f029d56c4a0bd528db351fcb9
Charli Russell <131972828+Charlirussell@users.noreply.github.com>
2024-03-12 16:00:45 +0100

    Drop down menu added for screen under 768px

--- ShadowSecurity/src/components/Navbar/Navbar.jsx
+++ ShadowSecurity/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,45 @@
+import { useState, useEffect } from 'react';
+
 import './Navbar.scss'
 
-function Navbar() {
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  };
+
+  const closeMenu = () => {
+    if (window.innerWidth > 768) {
+      setShowMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', closeMenu);
+    return () => {
+      window.removeEventListener('resize', closeMenu);
+    };
+  }, []);
+
+  const handleLinkClick = () => {
+    if (window.innerWidth <= 768) {
+      setShowMenu(false);
+    }
+  };
 
   return (
-    <>
-      <div>
-        Hello Navbar
+    <nav>
+      <div className={`dropdown ${showMenu ? 'show-menu' : ''}`} onClick={toggleMenu}>
+        <div className="dropdown-icon">&#x2630;</div>
+        <ul className="dropdown-menu">
+          <li onCLick={handleLinkClick}>Home</li>
+          <li onCLick={handleLinkClick}>Downloads</li>
+          <li onCLick={handleLinkClick}>About</li>
+          <li onCLick={handleLinkClick}>Contact</li>
+        </ul>
       </div>
-
-    </>
+    </nav>
   )
 }
 
