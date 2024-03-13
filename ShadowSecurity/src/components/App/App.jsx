acfa385ca3f99604cec047c52129c9c7953890f7
Charli Russell <131972828+Charlirussell@users.noreply.github.com>
2024-03-13 15:29:57 +0100

    Home page started, content boxes started and button added to index.scss

--- ShadowSecurity/src/components/App/App.jsx
+++ ShadowSecurity/src/components/App/App.jsx
@@ -2,6 +2,8 @@ import Header from '../Header/Header';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 
+import Home from '../Home/Home';
+
 import './App.scss'
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
         <div>
           <Header />
           <Navbar />
+          <Home />
           <Footer />
         </div>
 
---
+++ ShadowSecurity/src/components/Home/Home.jsx
@@ -0,0 +1,36 @@
+import './Home.scss'
+
+const Home = () => {
+
+
+  return (
+    <>
+      <div className='home'>
+        <h1>Shadow Security</h1>
+        <p className='h1p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus necessitatibus quo perspiciatis deleniti dolorum delectus voluptate, cumque veniam asperiores? Hic sed in dignissimos fuga rerum magni deleniti cum quaerat cupiditate.</p>
+        <button>Call Us</button>
+      <div className='container'>
+        <div className='boxone'>
+          <h2 className='h2boxleft'>Importance of Web Security</h2>
+          <p className='pboxleft'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis repellendus dolores deserunt ipsum consectetur labore officia delectus ullam rem, inventore quod aut dolore possimus temporibus voluptatibus. Quam eius inventore reiciendis.</p>
+        </div>
+        <div className='boxtwo'>
+          <h2 className='h2boxright'>Importance of Web Security</h2>
+          <p className='pboxright'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis repellendus dolores deserunt ipsum consectetur labore officia delectus ullam rem, inventore quod aut dolore possimus temporibus voluptatibus. Quam eius inventore reiciendis.</p>
+        </div>
+        <div className='boxthree'>
+          <h2 className='h2boxleft'>Importance of Web Security</h2>
+          <p className='pboxleft'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis repellendus dolores deserunt ipsum consectetur labore officia delectus ullam rem, inventore quod aut dolore possimus temporibus voluptatibus. Quam eius inventore reiciendis.</p>
+        </div>
+        <div className='boxfour'>
+          <h2 className='h2boxright'>Importance of Web Security</h2>
+          <p className='pboxright'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis repellendus dolores deserunt ipsum consectetur labore officia delectus ullam rem, inventore quod aut dolore possimus temporibus voluptatibus. Quam eius inventore reiciendis.</p>
+        </div>
+        </div>
+      </div>
+    
+    </>
+  )
+}
+
+export default Home
